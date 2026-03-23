3bc958b44f05665f9345f1bce43c3c1903335cd8
Alex <df-642@yandex.ru>
2026-03-23 22:47:58 +0700

    Add Example

--- src/main/java/com/eltex/Main.java
+++ src/main/java/com/eltex/Main.java
@@ -1,19 +1,31 @@
 package com.eltex;
 
 
+import com.eltex.exceptions.NotFoundException;
 import com.eltex.models.Attachment;
 import com.eltex.models.AttachmentType;
+import com.eltex.models.User;
+import com.eltex.service.UserService;
 import com.eltex.service.WallService;
 
 public class Main {
 
     public static void main(String[] args) {
-        final var service = new WallService();
-        service.like();
-        service.setAuthor("Автор");
-        service.setAttachment(new Attachment(null, AttachmentType.VIDEO));
-        System.out.println(service.getPost().attachment());
-        System.out.println(service.getPost());
-        System.out.println(service.getPost().original());
+        UserService userService = new UserService();
+
+        User user1 = new User(0, "login1", "name1", "avatar1");
+        User user2 = new User(1, "login2", "name2", "avatar2");
+
+        userService.save(user1);
+        userService.save(user2);
+        userService.save(user1);
+
+        try {
+            userService.removeById(0);
+        } catch (NotFoundException notFoundException) {
+            System.out.println(notFoundException);
+        }
+
+        System.out.println(userService.getAll());
     }
 }
